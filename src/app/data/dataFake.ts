980f4f44817598a3fee1d89d53c11bf811b4533a
Nivaldo Rodrigues <nivaldo_rodrigues@hotmail.com.br>
2023-03-17 09:01:35 -0300

    add dataFake, route and components

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id" : "1",
+    "title" : "Saiu a nova versão do Angular",
+    "description" : "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    "photoCover": "https://i.stack.imgur.com/luaq2.png"
+  },
+  {
+    "id" : "2",
+    "title" : "Saiu a nova versão do Angular",
+    "description" : "ola",
+    "photoCover": "https://criticalhits.com.br/wp-content/uploads/2019/10/tony-stark.jpg"
+  },
+  {
+    "id" : "3",
+    "title" : "Saiu a nova versão do Angular",
+    "description" : "ola",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id" : "4",
+    "title" : "Saiu a nova versão do Angular",
+    "description" : "ola",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+]
